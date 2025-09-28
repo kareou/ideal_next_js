f191a9563ca6c022de9e6d54809b2fd67db58112
unknown <adnanegu5@gmail.com>
2025-09-28 22:54:47 +0100

    adding a route to create users

---
+++ nextjs-clone/src/app/api/hdn/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+import  prisma  from "@/lib/prisma";
+import bcrypt from "bcrypt";
+
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { email, password } = body;
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and password are required" },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 409 }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name: email.split("@")[0], 
+      },
+    });
+
+    return NextResponse.json(
+      {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
